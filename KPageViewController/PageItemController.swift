220e9f15f8e52bfb00c4a28161087eaa5fee2630
Ky Nguyen <kynguyen@Kys-MacBook-Pro.local>
2016-01-12 22:38:27 +0700

    init project

---
+++ KPageViewController/PageItemController.swift
@@ -0,0 +1,45 @@
+//
+//  PageItemController.swift
+//  KPageViewController
+//
+//  Created by Ky Nguyen on 1/12/16.
+//  Copyright © 2016 Ky Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class PageItemController: UIViewController {
+
+    var itemIndex: Int = 0
+    var imageName: String = "" {
+        
+        didSet {
+            
+            if let imageView = contentImageView {
+                imageView.image = UIImage(named: imageName)
+            }
+            
+        }
+    }
+    
+    var placeName : String = "" {
+        
+        didSet {
+            if let place = placeNameLabel {
+                place.text = placeName
+            }
+        }
+    }
+    @IBOutlet weak var placeNameLabel: UILabel!
+    
+    @IBOutlet var contentImageView: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        contentImageView!.image = UIImage(named: imageName)
+        placeNameLabel!.text = placeName
+    }
+
+}
