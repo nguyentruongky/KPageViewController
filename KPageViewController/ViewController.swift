52b5bc945c147040644e2d3d4b136e30593266d8
Nguyen Truong Ky <nguyentruongky3390@yahoo.com>
2016-09-09 16:40:46 +0700

    new

--- KPageViewController/ViewController.swift
+++ KPageViewController/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController, PageViewDelegate {
-
+//first commit 
+//second commit
+//third commit
     private let contentImages = ["nature_pic_1.png",
         "nature_pic_2.png",
         "nature_pic_3.png",
