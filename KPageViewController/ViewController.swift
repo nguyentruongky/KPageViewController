77715c907154d0b61cb2adefa32456106207a6ba
Nguyen Truong Ky <nguyentruongky3390@yahoo.com>
2016-09-09 16:16:02 +0700

    Second commit

--- KPageViewController/ViewController.swift
+++ KPageViewController/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, PageViewDelegate {
 //first commit 
-    
+//second commit    
     private let contentImages = ["nature_pic_1.png",
         "nature_pic_2.png",
         "nature_pic_3.png",
